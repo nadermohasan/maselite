5d7660c4594bca402b5324398da2170d75b076b1
Nader Mohamed <77895309+nadermohasan@users.noreply.github.com>
2026-09-17 22:32:46 +0300

    Update QuizResult.jsx

--- src/pages/QuizResult.jsx
+++ src/pages/QuizResult.jsx
@@ -239,7 +239,7 @@ export default function QuizResult() {
             <line x1="19" y1="12" x2="5" y2="12"></line>
             <polyline points="12 19 5 12 12 5"></polyline>
           </svg>
-          <span>العودة للرئيسية</span>
+          <span>تسجيل الخروج</span>
         </button>
 
         <Footer />
